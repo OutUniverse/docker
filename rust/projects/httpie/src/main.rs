4bc509540bd9bde654ffb25c2d22326fee8298e1
Johnson <johnson.zhao@planetart.cn>
2022-10-12 20:33:16 +0800

    debug

--- rust/projects/httpie/src/main.rs
+++ rust/projects/httpie/src/main.rs
@@ -1,11 +1,14 @@
+use anyhow::{Result, anyhow, Ok};
 use clap::Parser;
+use reqwest::{Url, Client, header, Response};
+use std::{str::FromStr, collections::HashMap};
 
 /// A naive httpie implementation with Rust, can you imagine how easy it is?
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", author = "Tyr Chen <tyr@chen.com>")]
 struct Opts {
     #[clap(subcommand)]
-    command: SubCommand,
+    subcmd: SubCommand,
 }
 
 #[derive(Parser, Debug)]
@@ -16,17 +19,74 @@ enum SubCommand {
 
 #[derive(Parser, Debug)]
 struct Get {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
 }
 
 #[derive(Parser, Debug)]
 struct Post {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>,
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: Vec<KvPair>,
 }
 
-fn main() {
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split('=');
+        let err = || anyhow!(format!("Failed to parse {}", s));
+
+        Ok(Self {
+            k: parts.next().ok_or(err())?.to_string(),
+            v: parts.next().ok_or(err())?.to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", resp.text().await?);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
-    println!("test");
+    
+    let client = Client::new();
+
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
 }
