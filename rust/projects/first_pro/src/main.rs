c12ce6d1ee381025e9b64c40acc58592011d7789
Zhao Ji <693941639@qq.com>
2022-11-15 22:59:31 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    age: u8,
+    active: bool,
+    sign_in_count: u64,
+    email: String,
+}
+
 fn main() {
-    
+    let u1 = User {
+        username: String::from("User 1"),
+        age: 8,
+        active: true,
+        sign_in_count: 10,
+        email: String::from("johnson@gmail.com"),
+    };
+
+    let u2 = User {
+        username: String::from("User 2"),
+        ..u1
+    };
+
+    // println!("{:?}", u1);
 }
