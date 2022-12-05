d4b2e2fd77e3067d46edab1472f74095c9032713
Zhao Ji <693941639@qq.com>
2022-12-05 21:04:24 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,16 +1,94 @@
-use std::io;
+#[cfg(test)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[cfg(test)]
+enum Message {
+    Hello { id: i32 }
+}
 
 fn main() {
-    let a = [1; 5];
-    println!("Please input index:");
+    
+}
 
-    let mut index = String::new();
+#[cfg(test)]
+fn testAtGiveValueToVariable() {
+    match 12 {
+        num @ (12 | 13) => println!("{}", num),
+        _ => println!("Other"),
+    }
+}
 
-    io::stdin().read_line(&mut index).expect("Failed to read");
+#[cfg(test)]
+fn testAtForNewVariable() {
+    let msg = Message::Hello {id: 32};
 
-    let index: usize = index.trim().parse().expect("Index was not a number");
+    match msg {
+        Message::Hello { id: id @ 1..=21 } => {
+            println!("id is {}", id);
+        },
+        _ => {
+            println!("nothing");
+        }
+    }
+}
 
-    let element = a[index];
+#[cfg(test)]
+fn testOwnerForMatchMode() {
+    let s = Some(String::from("test"));
 
-    println!("The index {} value is {}", index, element);
+    if let Some(x) = &s {
+        println!("{}", x);
+    }
+
+    println!("{:?}", s);
+}
+
+#[cfg(test)]
+fn testDeconstruct() {
+    let p = Point { x: 1, y: 2 };
+
+    let Point {x: a, y: b}= p;
+
+    println!("x is {}, y is {}", a, b);
+}
+
+#[cfg(test)]
+fn testWhileLet() {
+    let mut a = Vec::new();
+
+    let b = ["One", "Two", "Three", "Four", "Five"];
+
+    for i in b {
+        a.push(i);
+    }
+
+    while let Some("Five") = a.pop() {
+        println!("This is Five");
+    }
+
+    println!("{:#?}", a);
+}
+
+#[cfg(test)]
+fn testIfLet() {
+    let a = Some("test if let");
+
+    if let Some("test if let") = a {
+        println!("a is {:?}", a);
+    }   
+}
+
+#[cfg(test)]
+fn testMatch() {
+    let mut a = Some(1);
+
+    a = match a {
+        Some(i) => Some(i + 1),
+        None => None,
+    };
+
+    println!("{:?}", a);
 }
