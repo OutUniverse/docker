e8a17eb702f8b6eda6c7715a0d6c3b7e1262e01e
Johnson <johnson.zhao@planetart.cn>
2023-01-09 17:15:42 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Test<'a> {
+    part: &'a str,
+}
 
+#[allow(dead_code)]
+fn main() {
+    let a = String::from("This is a test. You must believe");
+    let b = a.split(".").next().expect("could not find a \".\"");
+
+    let i = Test { part: b };
+
+    println!("{:#?}", i);
+}
+
+#[cfg(test)]
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
 }
