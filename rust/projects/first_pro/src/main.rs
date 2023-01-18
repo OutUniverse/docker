c6df1054e7d31de6d5f8adb110b24fbd075952ef
Johnson <johnson.zhao@planetart.cn>
2023-01-18 13:18:24 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,45 @@
+/* 使下面代码正常运行 */
+struct Interface<'a> {
+    manager: &'a mut Manager<'a>
+}
+
+impl<'a> Interface<'a> {
+    pub fn noop(self) {
+        println!("interface consumed");
+    }
+}
+
+struct Manager<'a> {
+    text: &'a str
+}
+
+struct List<'a> {
+    manager: Manager<'a>,
+}
+
+impl<'a> List<'a> {
+    pub fn get_interface(&'a mut self) -> Interface {
+        Interface {
+            manager: &mut self.manager
+        }
+    }
+}
+
 fn main() {
-    let a = "hello".to_owned();
+    let mut list = List {
+        manager: Manager {
+            text: "hello"
+        }
+    };
+    
+    use_list(&list);
+
+    list.get_interface().noop();
+
+    println!("Interface should be dropped here and the borrow released");
+
+}
+
+fn use_list(list: &List) {
+    println!("{}", list.manager.text);
 }
