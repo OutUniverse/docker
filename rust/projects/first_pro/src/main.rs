6f21ab27e025d70bc63007c49d6a19ed61028eaf
Johnson <johnson.zhao@planetart.cn>
2023-03-01 17:51:56 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,37 @@
+use std::ops::Deref;
+
 fn main() {
-    
+    struct MyBox<T>(T);
+
+    trait Test {
+        type Test;
+
+        fn test(self) -> Self::Test;
+    }
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    impl<T> Test for MyBox<T> {
+        type Test = T;
+
+        fn test(self) -> Self::Test {
+            self.0
+        }
+    }
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    let a = MyBox::new(1);
+
+    assert_eq!(1, *a);
 }
