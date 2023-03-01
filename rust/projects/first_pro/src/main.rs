b3b4c35db1cc0b393fc5ed05541e24e0971a0147
Zhao Ji <693941639@qq.com>
2023-03-01 22:39:54 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,6 +1,38 @@
-use std::ops::Deref;
-
 fn main() {
+    let s = String::from("test");
+
+    let s1: &str = &s;
+    let s2: String = s.to_string();
+
+    println!("{} {:?}", s1, s2);
+}
+
+#[cfg(test)]
+fn test_continuous_deref() {
+    let s = Box::new(String::from("test"));
+
+    fn test(a: &str) {
+        println!("{}", a);
+    }
+
+    test(&s);
+}
+
+#[cfg(test)]
+fn test_string_deref() {
+    let s = String::from("hello world");
+
+    fn test(a: &str) {
+        print!("{}", a);
+    }
+
+    test(&s);
+}
+
+#[cfg(test)]
+fn test_my_box() {
+    use std::ops::Deref;
+
     struct MyBox<T>(T);
 
     trait Test {
