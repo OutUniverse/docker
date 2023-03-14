a72d4a9a53de39b8186240c20d2b91fa37994378
Johnson <johnson.zhao@planetart.cn>
2023-03-14 10:05:53 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,4 +1,42 @@
+#[allow(dead_code)]
 fn main() {
+    use std::rc::Rc;
+
+    struct Owner {
+        name: String
+    }
+
+    struct Gadget {
+        name: String,
+        owner: Rc<Owner>
+    }
+
+    let owner_one = Rc::new(Owner { name: "owner one".to_string() });
+
+    let gadget_one = Gadget { name: "gadgetone".to_string(), owner: Rc::clone(&owner_one) };
+    let gadget_two = Gadget { name: "gadgettwo".to_string(), owner: Rc::clone(&owner_one) };
+
+    println!("All owner rc count {}", Rc::strong_count(&owner_one));
+
+    drop(owner_one);
+
+    println!("All owner rc count {}", Rc::strong_count(&gadget_one.owner));
+
+    drop(gadget_one);
+
+    println!("All owner rc count {}, owner name is {:?}", Rc::strong_count(&gadget_two.owner), gadget_two.owner.name);
+
+    println!("owner name is {:?}", gadget_two.owner.name);
+
+    fn test_rc_owner (_a: Rc<Owner>) {
+
+    }
+
+    test_rc_owner(gadget_two.owner);
+}
+
+#[cfg(test)]
+fn test_rc() {
     use std::rc::Rc;
 
     let a = Rc::new(String::from("test"));
@@ -15,5 +53,5 @@ fn main() {
         println!("{}", Rc::strong_count(&c));
     }
 
-    println!("{}", Rc::strong_count(&a));
+    println!("{}", Rc::strong_count(&a));    
 }
