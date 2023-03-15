b2efe9e17390d9600f3a76dda1514a57159009f8
Zhao Ji <693941639@qq.com>
2023-03-15 22:29:48 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    
+    use std::cell::RefCell;
+
+    let s = RefCell::new(String::from("test"));
+
+    let s1 = s.borrow();
+    let s2 = s.borrow_mut();
+
+    println!("{} {}", s1, s2);
+}
+
+#[cfg(test)]
+fn test_cell() {
+    use std::cell::Cell;
+
+    let c = Cell::new("aaa");
+
+    let one = c.get();
+
+    c.set("bbb");
+
+    let two = c.get();
+
+    println!("{} {}", one, two);
 }
