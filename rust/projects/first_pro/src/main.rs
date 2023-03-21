8a878074e8db30d6678db8eb2013a96decb60e45
Zhao Ji <693941639@qq.com>
2023-03-21 23:02:04 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,3 +1,30 @@
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil
+}
+
+impl List {
+    fn tail(&self) -> Option<&Rc<List>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None
+        }
+    }
+}
+
+struct A {
+    a: String
+}
+
+impl A {
+    fn test(&self) -> &String {
+        &self.a
+    }
+}
+
 fn main() {
     
 }
