9723f5ffc18111167ebf450c42476eaf7f4dfc94
Johnson <johnson.zhao@planetart.cn>
2023-04-24 17:08:35 +0800

    debug

--- rust/projects/first_pro/src/main.rs
+++ rust/projects/first_pro/src/main.rs
@@ -1,7 +1,51 @@
 
 
 fn main() {
-    
+    use std::cell::RefCell;
+    use std::thread;
+
+    thread_local!(static FOO: RefCell<u32> = RefCell::new(1));
+
+    FOO.with(|f| {
+        assert_eq!(*f.borrow(), 1);
+        *f.borrow_mut() = 2;
+    });
+
+    let spawned_thread = thread::spawn(move || {
+        FOO.with(|f| {
+            assert_eq!(*f.borrow(), 1);
+            *f.borrow_mut() = 3;
+            println!("{}", *f.borrow());
+        });
+    });
+
+    spawned_thread.join().unwrap();
+
+    FOO.with(|f| {
+        println!("{}", *f.borrow());
+    });
+}
+
+#[cfg(test)]
+fn test_barrier() {
+    use std::sync::{Arc, Barrier};
+    use std::thread;
+
+    let mut handles = Vec::with_capacity(6);
+    let barrier = Arc::new(Barrier::new(6));
+
+    for i in 0..6 {
+        let b = Arc::clone(&barrier);
+        handles.push(thread::spawn(move || {
+            println!("Thread {} started", i);
+            b.wait();
+            println!("Thread {} finished", i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
 
 #[cfg(test)]
@@ -34,7 +78,7 @@ fn test_spawn() {
 
     let handle = thread::spawn(move || {
         for i in 1..10 {
-            println!("This is No.{} thread run, v is {:?}", i, v);
+            println!("This is Number {} on on spawned thread, v is {:?}", i, v);
             thread::sleep(Duration::from_millis(1));
         }
     });
@@ -42,7 +86,7 @@ fn test_spawn() {
     handle.join().unwrap();
 
     for i in 1..5 {
-        println!("This is No.{} main thread run", i);
+        println!("This is Number {} on main thread run", i);
         thread::sleep(Duration::from_millis(1));
     }
 }
